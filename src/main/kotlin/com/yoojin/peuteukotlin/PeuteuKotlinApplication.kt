9e6cbf3b18f6c5d00aa04575990a5dfb513a7b66
Yoojin Kim <yoojinkim24@gmail.com>
2025-07-04 02:03:02 +0900

    Initial commit
    - HTML 화면 구성

---
+++ src/main/kotlin/com/yoojin/peuteukotlin/PeuteuKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.yoojin.peuteukotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PeuteuKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<PeuteuKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/yoojin/peuteukotlin/PeuteuKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yoojin.peuteukotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PeuteuKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
