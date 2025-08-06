b411895779b317e9535ddbebc3bdc86f803a5152
Yoojin Kim <yoojinkim24@gmail.com>
2025-08-06 23:52:52 +0900

    [FEAT] 프로틴 섭취 시간 항목 추가
    - 목록 조회, 수정, 등록 기능 수정

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -19,5 +19,22 @@ const COMMON = {
         const seconds = String(now.getSeconds()).padStart(2, '0');
 
         return `${COMMON.getTodayDate()}T${hours}:${minutes}:${seconds}`;
+    },
+    formatTime: (time) => {
+        const date = new Date(time);
+        const hours = date.getHours() % 12 ? date.getHours() % 12 : 12;
+        const ampm = date.getHours() >= 12 ? 'PM' : 'AM';
+        const minutes = String(date.getMinutes()).padStart(2, '0');
+        return `${String(hours).padStart(2, '0')}:${minutes} ${ampm}`;
+    },
+    parseDateAndTime: (dateStr, timeStr) => {
+        // 시간 파싱
+        const [time, modifier] = timeStr.split(" ");
+        let [hours, minutes] = time.split(":").map(Number);
+
+        if (modifier === "PM" && hours < 12) hours += 12;
+        if (modifier === "AM" && hours === 12) hours = 0;
+
+        return `${dateStr}T${hours}:${minutes}:00`;
     }
 }
